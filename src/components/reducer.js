6e6a2925fd660cba15ff0a5e5f8bdfa81abd149e
oguz gül <ogzgl95@outlook.com>
2021-08-27 22:34:41 +0300

    Build complated Body's header

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlist: [],
   playing: false,
   item: null,
-   token:
-    "3OVB3iYtgIF8qPxQpUL7Kou_2Xvmxt2h34PwCceJzWIaN79cHB90pWNm1gGNjagkelag7u7uuMT4bu8IyRdqBCyUIxQelFWDdORZzTWsDjxhu41iYOF0C4bJE0xg", 
+    token:
+    "3OVB3iYtgIF8qPxQpUL7Kou_2Xvmxt2h34PwCceJzWIaN79cHB90pWNm1gGNjagkelag7u7uuMT4bu8IyRdqBCyUIxQelFWDdORZzTWsDjxhu41iYOF0C4bJE0xg",  
 };
 
 const reducer = (state, action) => {
