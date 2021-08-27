65732c6636fe344f06886e4731b61350aa70b458
oguz gül <ogzgl95@outlook.com>
2021-08-27 22:02:15 +0300

    Build complated spotify play in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,53 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineOutlinedIcon from "@material-ui/icons/PlayCircleOutlineOutlined";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I'm the Footer</h1>
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/tr/4/40/600px-%285%29tarkan_-_karma_a.jpg"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Yeah</h4>
+          <p>User</p>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineOutlinedIcon
+          fontSize="large"
+          className="footer__icon"
+        />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={1}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item-xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
